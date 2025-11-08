7f1167a0a1b933e36fbc3835ae3e8911ba5e9063
UGAM <ugamgehlot@gmail.com>
2025-11-08 20:33:02 +0530

    add 37 Sudoku Solver problem in Backtracking category

---
+++ Backtracking/37_SudokuSolver.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    bool isSafe(vector<vector<char>>& board,int row,int col,char dig){
+        for(int j=0;j<9;j++){
+            if(board[row][j]==dig){
+                return false;
+            }
+        }
+        for(int i=0;i<9;i++){
+            if(board[i][col]==dig){
+                return false;
+            }
+        }
+        int srow=(row/3)*3;
+        int scol=(col/3)*3;
+        for(int i=srow;i<=srow+2;i++){
+            for(int j=scol;j<=scol+2;j++){
+                if(board[i][j]==dig){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool helper(vector<vector<char>>& board,int row,int col){
+        if(row==9){
+            return true;
+        }
+        int nxtrow=row,nxtcol=col+1;
+        if(col==9){
+            nxtrow=row+1;
+            nxtcol=0;
+        }
+        if(board[row][col]!='.'){
+            return helper(board,nxtrow,nxtcol);
+        }
+        for(char dig='1';dig<='9';dig++){
+            if(isSafe(board,row,col,dig)){
+                board[row][col]=dig;
+                if(helper(board,nxtrow,nxtcol)){
+                    return true;
+                }
+                board[row][col]='.';
+
+            }
+        }
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board,0,0);
+    }
+};
